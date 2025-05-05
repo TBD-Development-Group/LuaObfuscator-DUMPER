b695e39d9aea92d1746d6ff48dec7374da290726
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-05 19:05:37 +0200

    Update runner.py

--- dumper/core/runner.py
+++ dumper/core/runner.py
@@ -1,1 +1,15 @@
+from lupa import LuaRuntime
+
+def run_lua(code):
+    lua = LuaRuntime(unpack_returned_tuples=True)
+    output_lines = []
+
+    def capture_print(*args):
+        output_lines.append(" ".join(map(str, args)))
+
+    lua.globals().print = capture_print
+
+    try:
+        lua.execute(code)
+    except
 
