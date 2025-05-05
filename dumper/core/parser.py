f7159903eaee9759941fa370833bb1a1f9d35c12
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-05 19:06:18 +0200

    Update parser.py

--- dumper/core/parser.py
+++ dumper/core/parser.py
@@ -1,1 +1,7 @@
+def extract_functions(code):
+    """
+    Returns list of function names defined in the Lua code.
+    """
+    import re
+    return re.findall(r'(?:local\s+)?function\s+([a-zA-Z0-9_\.]+)', code)
 
